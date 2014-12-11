a7c0479b9e3c796a7f806f238fa41d184c3d0434
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2014-12-11 20:21:45 +0100

    Cámara con ratón
    Se ha añadido el script MouseLook de los assets de Unity para hacer que
    la cámara se mueva con el movimiento del ratón.
    Los controles actualmente son, rotación de la cámara: movimiento del
    ratón, movimiento de la cámara: WASD.

--- Traffic Flow Simulator/Assets/Scripts/MainCameraController.cs
+++ Traffic Flow Simulator/Assets/Scripts/MainCameraController.cs
@@ -3,16 +3,15 @@ using System.Collections;
 
 public class MainCameraController : MonoBehaviour {
 
-	public float vertical_velocity = 4f;
 	public float movement_velocity = 4f;
-	public float rotation_velocity = 20f;
+	
+	void Start () {
+		Screen.showCursor = false;
+	}
 
 	void Update () {
-		this.transform.Translate (Input.GetAxis("Vertical") * Vector3.up * Time.deltaTime * vertical_velocity, this.transform);
 		this.transform.Translate (Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * movement_velocity, this.transform);
-		this.transform.Translate (Input.GetAxis("Zoom") * Vector3.forward * Time.deltaTime * movement_velocity, this.transform);
-		this.transform.Rotate (0, Input.GetAxis("RotHorizontal") * Time.deltaTime * rotation_velocity, 0, Space.World);
-		this.transform.Rotate (Input.GetAxis("RotVertical") * Time.deltaTime * rotation_velocity, 0, 0, Space.Self);
+		this.transform.Translate (Input.GetAxis("Forward") * Vector3.forward * Time.deltaTime * movement_velocity, this.transform);
 	}
 
 	public void goTo (float x, float y, float z) {
