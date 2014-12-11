4f1cf943042700ffe6119cdbad3d81a8a0d7593b
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2014-12-12 00:26:16 +0100

    Modificador velocidad cámara
    Añadido los modificadores de velocidad de desplazamiento de cámara: Left
    Shift para aumentar la velocidad, Left Control para disminuir la
    velocidad.

--- Traffic Flow Simulator/Assets/Scripts/MainCameraController.cs
+++ Traffic Flow Simulator/Assets/Scripts/MainCameraController.cs
@@ -4,14 +4,31 @@ using System.Collections;
 public class MainCameraController : MonoBehaviour {
 
 	public float movement_velocity = 4f;
+	public float movement_modifier = 3f;
+
+	private float speed;
 	
 	void Start () {
 		Screen.showCursor = false;
+		speed = movement_velocity;
 	}
 
 	void Update () {
-		this.transform.Translate (Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * movement_velocity, this.transform);
-		this.transform.Translate (Input.GetAxis("Forward") * Vector3.forward * Time.deltaTime * movement_velocity, this.transform);
+	
+		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+			speed = movement_velocity * movement_modifier;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.LeftControl)) {
+			speed = movement_velocity / movement_modifier;
+		}
+		
+		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.LeftControl)) {
+			speed = movement_velocity;
+		}
+	
+		this.transform.Translate (Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * speed, this.transform);
+		this.transform.Translate (Input.GetAxis("Forward") * Vector3.forward * Time.deltaTime * speed, this.transform);
 	}
 
 	public void goTo (float x, float y, float z) {
