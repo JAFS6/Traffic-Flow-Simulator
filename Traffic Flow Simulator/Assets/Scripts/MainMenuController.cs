962ee49bcda5c1a665b0f6f589c8ca8fb93407b1
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2015-01-28 20:55:52 +0100

    Botones cargar mapa
    En el panel de start se colocan tantos botones como archivos que no
    acaben en .meta haya en la carpeta Application.dataPath +
    "/Resources/Maps/"

--- Traffic Flow Simulator/Assets/Scripts/MainMenuController.cs
+++ Traffic Flow Simulator/Assets/Scripts/MainMenuController.cs
@@ -1,14 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
 	public void showStart () {
+		// Eliminar los posibles botones de carga de mapa anteriores
+		GameObject [] buttons = GameObject.FindGameObjectsWithTag ("LoadMapButton");
+		
+		foreach (GameObject item in buttons) {
+			Destroy(item);
+		}
+	
 		// Ocultar los otros paneles
 		hideOptionsPanel();
 		hideCreditsPanel();
 		// Mostrar el panel de inicio
 		showStartPanel();
+		
+		// Buscar los mapas disponibles y añadir un boton por cada uno de ellos
+		DirectoryInfo info = new DirectoryInfo(Application.dataPath + "/Resources/Maps/");
+		FileInfo [] fileInfo = info.GetFiles();
+		float my_y = 0;
+		float i = 0;
+		
+		foreach (FileInfo file in fileInfo) {
+			if (!file.Name.Contains("meta")) {
+				string filename_w_extension = file.Name;
+				string [] split = filename_w_extension.Split(new char[] {'.'});
+				string filename = split[0];
+				GameObject start_panel = GameObject.Find("Start Panel");
+				GameObject example_button = GameObject.Find("ExampleButton");
+				
+				if (example_button == null) {
+					Debug.LogError("ExampleButton not found");
+				}
+				else {
+					GameObject button = (GameObject)GameObject.Instantiate(example_button,new Vector3(0,0,0),Quaternion.identity);
+					button.transform.SetParent(start_panel.transform);
+					button.name = "LoadMapButton"+"_"+i;
+					button.tag = "LoadMapButton";
+					button.transform.localScale = new Vector3(1,1,1);
+					button.transform.position = new Vector3(start_panel.transform.position.x,start_panel.transform.position.y - my_y,0);
+					button.transform.Find("Text").GetComponent<Text>().text = filename;
+					
+					my_y += 30;
+					i++;
+				}
+			}
+		}
 	}
 	
 	public void showOptions () {
