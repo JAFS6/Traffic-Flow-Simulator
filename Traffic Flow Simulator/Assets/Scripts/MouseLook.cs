62dd9286b6a6fcc7238668dcc6f81448e1c73da2
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2015-02-24 23:42:28 +0100

    Issue #100 fixed
    Also fixed pause panel interactivity and minimun camera position

--- Traffic Flow Simulator/Assets/Scripts/MouseLook.cs
+++ Traffic Flow Simulator/Assets/Scripts/MouseLook.cs
@@ -33,25 +33,29 @@ public class MouseLook : MonoBehaviour {
 	void Update ()
 	{
 		if (!SimulationUIController.is_paused) {
-			if (axes == RotationAxes.MouseXAndY)
-			{
-				float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+		
+			if (MainCameraController.can_move) {
 				
-				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-				
-				transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-			}
-			else if (axes == RotationAxes.MouseX)
-			{
-				transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-			}
-			else
-			{
-				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-				
-				transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+				if (axes == RotationAxes.MouseXAndY)
+				{
+					float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+					
+					rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+					rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+					
+					transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+				}
+				else if (axes == RotationAxes.MouseX)
+				{
+					transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+				}
+				else
+				{
+					rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+					rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+					
+					transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+				}
 			}
 		}
 	}
