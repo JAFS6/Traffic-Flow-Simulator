8187e628bcd076447a73055792b7b8ed59d8ff77
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2015-03-24 19:17:55 +0100

    Arrows at the start of each normal lane

--- Traffic Flow Simulator/Assets/Scripts/RoadMap.cs
+++ Traffic Flow Simulator/Assets/Scripts/RoadMap.cs
@@ -869,6 +869,7 @@ public static class RoadMap {
 		// Lane lines
 
 		Vector3 save_position = new Vector3 (position.x, position.y, position.z);
+		GameObject straight_arrow_prefab = Resources.Load("Prefabs/straight_arrow", typeof(GameObject)) as GameObject;
 
 		// Paint as many lines as lanes are in each direction except one 
 		// and put as many start lane as lanes have
@@ -887,6 +888,12 @@ public static class RoadMap {
 					draw_lane_line (lane_type, e.length, position, platform);
 				}
 				setLaneStartPoint (lane_type, new Vector3 (position.x + (Constants.lane_width/2), position.y, position.z - (e.length/2)), source_start_points);
+				
+				if (lane_type == Constants.Char_Normal_Lane) {
+					Vector3 arrow_pos = new Vector3(position.x + (Constants.lane_width/2), position.y, position.z - ((e.length / 2) - 4f));
+					GameObject arrow = GameObject.Instantiate (straight_arrow_prefab, arrow_pos, Quaternion.identity) as GameObject;
+					arrow.transform.SetParent(platform.transform);
+				}
 			}
 
 			// Stop lines before intersection
@@ -913,6 +920,12 @@ public static class RoadMap {
 					draw_lane_line (lane_type, e.length, position, platform);
 				}
 				setLaneStartPoint (lane_type, new Vector3 (position.x - (Constants.lane_width/2), position.y, position.z + (e.length/2)), destination_start_points);
+				
+				if (lane_type == Constants.Char_Normal_Lane) {
+					Vector3 arrow_pos = new Vector3(position.x - (Constants.lane_width/2), position.y, position.z + ((e.length / 2) - 4f));
+					GameObject arrow = GameObject.Instantiate (straight_arrow_prefab, arrow_pos, Quaternion.AngleAxis(180, Vector3.up)) as GameObject;
+					arrow.transform.SetParent(platform.transform);
+				}
 			}
 
 			// Stop lines before intersection
