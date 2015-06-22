c0f0275b95c2a09b3f1bccc2c5a6d746fce7ea5a
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2015-06-22 09:16:05 +0200

    Issue #125 fixed. Help panel.

---
+++ Traffic Flow Simulator/Assets/Scripts/Shortcut.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Shortcut : MonoBehaviour
+{	
+	[SerializeField]
+	private KeyCode key;
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(key))
+		{
+			this.GetComponent<Toggle>().isOn = !this.GetComponent<Toggle>().isOn;
+		}
+	}
+}
